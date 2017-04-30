cc0ef4b132a15acd9e8c8694c7dbf573bcb7d84c
Satish Rathore <Satish Rathore>
2017-04-30 10:06:12 +0530

    satish update file 2

--- Spring_1/src/A.java
+++ Spring_1/src/A.java
@@ -4,7 +4,7 @@ public class A {
 	public void m(){
 		i+=i;
 		System.out.println(i);
-		System.out.println("i did some cnanged");
+		System.out.println("i did some changed");
 		System.out.println("satish rathore");
 	}
 	public static void main(String[] args) {
